6978b797f7b0222d08a4dfb691822851a0b0f481
Michaela Ockova <michaela.ockova@rossum.ai>
2021-05-18 08:50:26 +0200

    Bump version: 3.9.0 → 3.10.0

--- rossum/__init__.py
+++ rossum/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "3.9.0"
+__version__ = "3.10.0"
 
 CTX_PROFILE = "PROFILE"
 CTX_DEFAULT_PROFILE = "default"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rossum",
-    version="3.9.0",
+    version="3.10.0",
     description="Command line interface for controlling the Rossum platform",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",

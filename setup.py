f6b0573cf1f69e14249f440d148b7b9b176776dd
Jonas Amrich <jonas.amrich@rossum.ai>
2022-02-16 18:13:54 +0100

    Ensure compatibility with click 8.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(exclude=("tests*",)),
     install_requires=[
         "pandas",
-        "click<=7.1.2",
+        "click<8.1.0",
         "click-shell",
         'xlrd > 1.2.0;python_version>"3.6"',
         'xlrd==1.2.0;python_version=="3.6"',

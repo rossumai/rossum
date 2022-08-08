e5cdab7791f38001fc672669cda0e57c07fdfba2
Lukas Benka <lukas.benka@rossum.ai>
2022-08-08 10:27:35 +0000

    Bump version: 3.14.0 → 3.15.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rossum",
-    version="3.14.0",
+    version="3.15.0",
     description="Command line interface for controlling the Rossum platform",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
